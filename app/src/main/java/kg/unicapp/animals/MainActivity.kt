123a687bdb8e4d7795d9ade1da093f5a8e531219
agamonch <jnkyrgyz@gmail.com>
2021-04-12 23:11:17 +0300

    added function onclicklistener to pictures in MainActivity

--- app/src/main/java/kg/unicapp/animals/MainActivity.kt
+++ app/src/main/java/kg/unicapp/animals/MainActivity.kt
@@ -2,10 +2,28 @@ package kg.unicapp.animals
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val textClick = findViewById<TextView>(R.id.textView3)
+        val imgCat =findViewById<ImageView>(R.id.imgCat)
+        val imgHorse = findViewById<ImageView>(R.id.imgHorse)
+        val imgChick = findViewById<ImageView>(R.id.imgChick)
+        imgCat.setOnClickListener {
+            textClick.text = "Cat"  }
+        imgHorse.setOnClickListener {
+            textClick.text = "Horse"  }
+        imgChick.setOnClickListener {
+            textClick.text = "Chicken"  }
+
+
+
+
+
+
     }
 }
